3dcbc3f27578248be14b12fa6d764d6ceee4324b
SHAKTIPRASADDASH-25BCSD92 <dashshakti07@gmail.com>
2026-05-26 23:24:54 +0530

    Add files via upload

---
+++ day1/resturantbill.java
@@ -0,0 +1,19 @@
+package java_internship_assignment;
+
+public class resturantbill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****bill*******\n");
+		System.out.println("RESTURANT NAME-NOLIMITS\n");
+		System.out.println("****FOODITEMS*****\n");
+		System.out.println("PASTA-Rs400\n");
+		System.out.println("BURGER-Rs350\n");
+		System.out.println("CHIKENWINGS-Rs700\n");
+		System.out.println("*****GRANDTOTAL*******\n");
+		System.out.println("TOTAL AMOUNT-Rs1450\n");
+		System.out.println("Thanks For Visiting!!\n");
+		System.out.println("Rate your experience\n");
+	}
+
+}
