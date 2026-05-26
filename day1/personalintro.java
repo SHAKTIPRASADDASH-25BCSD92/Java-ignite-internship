ca24c6083fcaacab3bb3f6451d452332bc3fc46a
SHAKTIPRASADDASH-25BCSD92 <dashshakti07@gmail.com>
2026-05-26 23:13:33 +0530

    Add files via upload

---
+++ day1/personalintro.java
@@ -0,0 +1,10 @@
+package java_internship_assignment;
+
+public class personalintro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("NAME-SHAKTI PRASAD DASH\n BRANCH-CSE\nHOBBIES-play basketball");
+	}
+
+}
